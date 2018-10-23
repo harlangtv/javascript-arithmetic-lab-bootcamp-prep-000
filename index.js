ab650860129087607059ad6f7abe9a85e54f29b3
Harlan Green-Taub <harlan.gt@gmail.com>
2018-10-23 16:41:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,12 @@ function dec(n){
   return (-- n)
 }
 function makeInt(n){
+<<<<<<< HEAD
   return (parseInt(n, 10))
 }
 function preserveDecimal(n){
   return (parseFloat(n))
+=======
+  return (parseInt('n', 10))
+>>>>>>> b6f039e1f076a0f1d45d566b5cd5c95a52d51db4
 }
